833f484de080c172fe41992a9bb0b1f9b09b5477
Erictsl2000 <tan_shing_li@hotmail.com>
2026-06-08 01:23:09 +0800

    Update rider runtime version

--- apps/rider/app.config.ts
+++ apps/rider/app.config.ts
@@ -5,7 +5,7 @@ const mapsKey = process.env.GOOGLE_MAPS_API_KEY ?? '';
 const config: ExpoConfig = {
   name: 'Teeko',
   slug: 'teeko-rider',
-  version: '0.0.1',
+  version: '0.0.2',
   orientation: 'portrait',
   scheme: 'teeko',
   userInterfaceStyle: 'light',
